2e959c2b15313c83e1d2db8b17c6cd8cfbbb36ce
Evren <evrenyigit20@gmail.com>
2024-01-03 09:56:42 +0300

    entity klasoru eklendi

---
+++ src/main/java/mindtro/helloworld/entity/OperationLog.java
@@ -0,0 +1,29 @@
+package mindtro.helloworld.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name ="OperationLogs")
+public class OperationLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "package_name")
+    private String packageName;
+    @Column(name = "method_name")
+    private String methodName;
+    @Column(name = "date")
+    private Date date;
+    @Column(name = "client_IP")
+    private String clientIP;
+    @Column(name = "client_browser")
+    private String clientBrowser;
+    @Column(name = "operating_system")
+    private String operatingSystem;
+}
